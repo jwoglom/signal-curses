9a226aa6568f641b54cdf770b90738d4e347a4cf
James Woglom <j@wogloms.net>
2018-04-28 17:31:30 -0400

    messages display

--- main.py
+++ main.py
@@ -12,6 +12,39 @@ class MainForm(npyscreen.Popup):
 		print('after editing', self.to.value, self.msg.value)
 		self.parentApp.setNextForm('DONE')
 
+class MessagesLine(npyscreen.MultiLine):
+	_size = 15
+	_size_max = 30
+	def __init__(self, *args, **kwargs):
+		#kwargs['columns'] = 6
+		#kwargs['column_width'] = 20
+		super(MessagesLine, self).__init__(*args, **kwargs)
+
+	def update(self, *args, **kwargs):
+		self._gen_size()
+		super(MessagesLine, self).update(*args, **kwargs)
+
+	def _gen_size(self):
+		self._size = min(max([len(i[0]) for i in self.values]), self._size_max)
+
+	def display_value(self, val):
+		return self._gen_line(val, self._size, self._size_max)
+
+
+	def _gen_line_max(self, val, size_max):
+		if len(val) < size_max:
+			return val
+		return val[:size_max-3]+'...'
+
+	def _gen_line(self, val, size, size_max):
+		fmt = '{:>'+str(size)+'} | {}'
+		out = fmt.format(self._gen_line_max(val[0], size_max), val[1])
+		#out = str(self.width)+' '+str(len(out))+' '+out
+		
+
+		return out
+
+
 class AppForm(npyscreen.FormMuttActiveTraditionalWithMenus):
 	"""def create(self):
 		self.wStatus1 = self.add(npyscreen.TitleText, name='status1')
@@ -22,14 +55,35 @@ class AppForm(npyscreen.FormMuttActiveTraditionalWithMenus):
 	COMMAND_WIDGET_CLASS = npyscreen.TitleText
 	COMMAND_WIDGET_NAME = 'Send: '
 	COMMAND_WIDGET_BEGIN_ENTRY_AT = 1
+	MAIN_WIDGET_CLASS = MessagesLine
 
-	MAIN_WIDGET_CLASS = npyscreen.SimpleGrid
+	def create(self):
+		self.m1 = self.add_menu(name="Main Menu", shortcut="^X")
+		self.m1.addItemsFromList([
+			("Display Text", self.whenDisplayText, None, None, ("some text",)),
+			("Exit", self.whenExit, "e"),
+		])
+
+		super(AppForm, self).create()
+
+
+	def whenDisplayText(self, arg):
+		npyscreen.notify_confirm(arg)
+
+	def whenExit(self):
+		self.parentApp.setNextForm(None)
+		self.editing = False
+		self.parentApp.switchFormNow()
 
 	def beforeEditing(self):
 		self.wMain.always_show_cursor = False
 		self.wMain.values = [
-			('from', 'foo'),
-			('from', 'bar')]
+			('John Smith', 'great weather huh? how are you doing'),
+			('Jane', 'foobarfoobar asdf asdf asdf asdf asdf asdf asdf '),
+			('Almosttoolong butnottoolong a', 'test foo'),
+			('Longlonglong Tootoolonglonglong', 'test blah'),
+			('John Smith', 'too long '*30),
+			('After', 'after text')]
 		self.wMain.display()
 
 		self.wStatus1.value = 'Title'
