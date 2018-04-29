efd0f0d20a88925719b817fcab2043d8f3056c95
James Woglom <j@wogloms.net>
2018-04-29 02:04:02 -0400

    message thread

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import curses
 import subprocess
 import threading
 import re
-from contextlib import redirect_stdout
+from queue import Queue
 from datetime import datetime
 
 CURSES_OTHER_ENTER = 10
+from secret import SELF_PHONE, TO_PHONE
 
 log_file = open('sc.log', 'w')
 log_file_lock = threading.Lock()
@@ -124,12 +125,20 @@ class AppMessageBox(npyscreen.TitleText):
 		self.parent.parentApp.queue_event(npyscreen.Event("RELOAD"))
 
 
+class AppState(object):
+	toName = None
+	toNumber = None
+
+	def __str__(self):
+		return 'state name:{} num:{}'.format(self.toName, self.toNumber)
+
 
 class AppForm(npyscreen.FormMuttActiveTraditionalWithMenus):
 	COMMAND_WIDGET_CLASS = AppMessageBox
 	COMMAND_WIDGET_NAME = 'Send: '
 	COMMAND_WIDGET_BEGIN_ENTRY_AT = 1
 	MAIN_WIDGET_CLASS = MessagesLine
+	state = None
 
 	def create(self):
 		self.m1 = self.add_menu(name="Main Menu", shortcut="^X")
@@ -140,11 +149,27 @@ class AppForm(npyscreen.FormMuttActiveTraditionalWithMenus):
 		])
 
 		self.add_event_hander("RELOAD", self.reloadHandler)
+		self.add_event_hander("SEND", self.sendHandler)
+
+		self.state = AppState()
+		self.state.toNumber = TO_PHONE
+
 		super(AppForm, self).create()
 
 	def reloadHandler(self, event):
 		#log('reloadHandler')
 		self.wMain.update()
+
+	def sendHandler(self, event):
+		val = self.wCommand.entry_widget.value
+		log('sendHandler', val)
+		self.wCommand.entry_widget.value = ''
+		self.wCommand.entry_widget.clear()
+
+		self.parentApp.message_queue.put({
+			'state': self.state,
+			'message': val
+		})
 
 	def whenAddLines(self, arg):
 		self.wMain.addValues([
@@ -158,6 +183,7 @@ class AppForm(npyscreen.FormMuttActiveTraditionalWithMenus):
 		self.parentApp.setNextForm(None)
 		self.editing = False
 		self.parentApp.switchFormNow()
+		exit(0)
 
 	def beforeEditing(self):
 		self.wMain.always_show_cursor = False
@@ -177,7 +203,10 @@ class AppForm(npyscreen.FormMuttActiveTraditionalWithMenus):
 class SignalApp(npyscreen.StandardApp):
 	app = None
 	daemonThread = None
+	messageThread = None
+	message_queue = Queue()
 	lines = []
+	messageLines = []
 	lineState = None
 	def onStart(self):
 		#self.addForm('MAIN', MainForm, name='Enter Message')
@@ -196,13 +225,17 @@ class SignalApp(npyscreen.StandardApp):
 		self.daemonThread = SignalDaemonThread(self.app)
 		self.daemonThread.start()
 
-	def handleLine(self, line):
+		self.messageThread = SignalMessageThread(self.app, self.message_queue)
+		self.messageThread.start()
+
+	def handleDaemonLine(self, line):
 		self.lines.append(line)
-		log('handleLine', line)
+		log('handleDaemonLine', line)
 		regexes = {
 			'init': r'Envelope from: \“([\w\d\s]*)\” (\+\d+) \(device: (\d+)\)',
 			'timestamp': r'Timestamp: (\d+) \((.*)\)',
 			'msgTimestamp': r'Message timestamp: (\d+) \((.*)\)',
+			'toLine': r'To: \“([\w\d\s]*)\” (\+\d+) , Message timestamp: (\d+) \((.*)\)',
 			'body': r'Body: (.*)'
 		}
 
@@ -222,6 +255,10 @@ class SignalApp(npyscreen.StandardApp):
 				self.lineState.timestamp = match[0]
 			elif typ == 'messageTimestamp':
 				self.lineState.msgTimestamp = match[0]
+			elif typ == 'toLine':
+				self.lineState.toName = match[0]
+				self.lineState.toNumber = match[1]
+				self.lineState.msgTimestamp = match[2]
 			elif typ == 'body':
 				self.lineState.messageBody = match[0]
 
@@ -234,9 +271,17 @@ class SignalApp(npyscreen.StandardApp):
 
 		#self.app.wMain.addValues([('*', line)])
 
+	def handleMessageLine(self, line):
+		self.messageLines.append(line)
+		log('handleMessageLine', line)
+
 class LineState(object):
 	fromName = None
 	fromNumber = None
+
+	toName = None
+	toNumber = None
+
 	timestamp = None
 	msgTimestamp = None
 	messageBody = None
@@ -266,16 +311,45 @@ class SignalDaemonThread(threading.Thread):
 
 	def run(self):
 		log('daemon thread')
-		#script = ["dbus-launch", "signal-cli", "-u '***REMOVED***'", "daemon"]
-		script = ['python3', 'sp.py']
+		script = ['signal-cli', '-u', SELF_PHONE, 'daemon']
+		#script = ['python3', 'sp.py']
 		for line in execute(script):
 			#log('queue event')
-			self.app.parentApp.handleLine(line)
+			self.app.parentApp.handleDaemonLine(line)
 			self.app.parentApp.queue_event(npyscreen.Event("RELOAD"))
 
 		log('daemon exit')
 
+class SignalMessageThread(threading.Thread):
+	app = None
+	daemon = False
+	queue = None
+	def __init__(self, app, queue):
+		super(SignalMessageThread, self).__init__()
+		self.app = app
+		self.queue = queue
 
+	def run(self):
+		log('message thread')
+		while True:
+			item = self.queue.get()
+			log('queue item', item)
+			self.do_action(**item)
+			self.queue.task_done()
+		log('message thread exit')
+
+	def do_action(self, state=None, message=None):
+		self.send_message(state.toNumber, message)
+
+	def send_message(self, number, message):
+		log('send_message', number, message)
+		script = ['signal-cli', '--dbus', 'send', str(number), '-m', message]
+		for line in execute(script):
+			#log('queue event')
+			self.app.parentApp.handleMessageLine(line)
+			self.app.parentApp.queue_event(npyscreen.Event("RELOAD"))
+
+		log('send_message done')
 
 if __name__ == '__main__':
 	signal = SignalApp()
